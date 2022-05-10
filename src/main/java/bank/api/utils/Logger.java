7e570bf59fdfcb046aff7e075c9002a117800b35
Eugene <eugenetkachevs@gmail.com>
2022-05-10 17:32:18 +0500

    refactor

--- src/main/java/bank/api/utils/Logger.java
+++ src/main/java/bank/api/utils/Logger.java
@@ -5,7 +5,6 @@ import bank.api.exceptions.WrongArgumentException;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +12,12 @@ import java.util.Arrays;
 
 @Aspect
 @Component
-public class LoggingAspect {
+public class Logger {
 
     @Around("@within(org.springframework.stereotype.Service) || @within(org.springframework.web.bind.annotation.RestController)")
     public Object logAroundServicesAndControllers(ProceedingJoinPoint pjp) {
 
-        Logger logger = LoggerFactory.getLogger(pjp.getTarget().getClass().getSimpleName() + ".class");
+        org.slf4j.Logger logger = LoggerFactory.getLogger(pjp.getTarget().getClass().getSimpleName() + ".class");
 
         try {
             logger.info(pjp.getTarget().getClass().getSimpleName() + "." + pjp.getSignature().getName() + " start with args: " + Arrays.toString(pjp.getArgs()));
@@ -29,10 +28,10 @@ public class LoggingAspect {
 
             return result;
         } catch (NotFoundException e) {
-            logger.error(pjp.getTarget().getClass().getSimpleName() + "." + pjp.getSignature().getName() + ": " + e.getMessage());
+            logger.error(pjp.getTarget().getClass().getSimpleName() + "." + pjp.getSignature().getName() + ": Error message = " + e.getMessage());
             throw new NotFoundException(e.getMessage());
         } catch (IllegalArgumentException e) {
-            logger.error(pjp.getTarget().getClass().getSimpleName() + "." + pjp.getSignature().getName() + ": " + e.getMessage());
+            logger.error(pjp.getTarget().getClass().getSimpleName() + "." + pjp.getSignature().getName() + ": Error message = " + e.getMessage());
             throw new WrongArgumentException(e.getMessage(), e);
         } catch (Throwable e) {
             throw new RuntimeException(e);
